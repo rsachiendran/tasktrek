0c4570fa0fb41499ee2830d59a556981d3c12422
Sachien <112466842+rsachiendran@users.noreply.github.com>
2024-08-12 13:35:25 +0530

    first commit

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const CreateTodo = () => {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const tasks = ["Task 1", "Task 2", "Task 3"];
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <>
+      <h1>Tasks: {count}</h1>
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleClick} value="Add Task Button">
+        Add Task
+      </button>
+      <h1>Input Text: {input}</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task}>{task}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default CreateTodo;
